f1a2c556207dbafaf41f50ac8c1fd4dec4bbc3a9
Tai Bui <tai.c.bui@gmail.com>
2023-12-13 17:56:38 +0700

    update

--- server/server.js
+++ server/server.js
@@ -1,11 +1,46 @@
-import express from 'express';
+const express = require('express');
+const pool = require('./db');
+const port = 3000;
+
 const app = express();
-const port = process.env.PORT || 3000;
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send(`Welcome to Tai's server!`);
+//routes
+app.get('/', async (req, res) => {
+  try {
+    const data = await pool.query('SELECT * FROM schools');
+    res.status(200).send(data.rows);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
 });
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+app.post('/', async (req, res) => {
+  const { name, location } = req.body;
+  console.log('req.body', req.body);
+  try {
+    await pool.query('INSERT INTO schools (name, address) VALUES ($1, $2)', [
+      name,
+      location,
+    ]);
+    res.status(200).send({ message: 'Successfully added child' });
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
 });
+
+app.get('/setup', async (req, res) => {
+  try {
+    await pool.query(
+      'CREATE TABLE schools( id SERIAL PRIMARY KEY, name VARCHAR(100), address VARCHAR(100))'
+    );
+    res.status(200).send({ message: 'Successfully created table' });
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
+});
+
+app.listen(port, () => console.log(`Server has started on port: ${port}`));
